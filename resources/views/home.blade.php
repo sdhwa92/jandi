4c25c0bfb330413019d8c4c5223454fda38eb08d
Daehwa Seo <sdhwa92@gmail.com>
2020-12-16 23:20:26 +1000

    Minor updates
    - Only 10 events on the list page (homepage)
    - Remove some unnecessary contents

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container home-page">
-    @if(Auth::check())
+    {{-- @if(Auth::check())
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -20,7 +20,7 @@
             </div>
         </div>
     </div>
-    @endif
+    @endif --}}
 
     <div class="row d-flex justify-content-center mt-5">
         <div class="col-md-8">
@@ -36,9 +36,14 @@
                 @foreach($events as $event)
                 <div class="mt-3 event-list">
                   <div class="d-flex justify-content-between align-items-center">
-                    <div class="d-flex flex-row align-items-center"> <span class="star"><i class="fa fa-futbol-o blue"></i></span>
+                    <div class="d-flex flex-row align-items-center"> 
+                      {{-- <span class="star"><i class="fa fa-futbol-o blue"></i></span> --}}
                       <div class="d-flex flex-column"> <span>{{ $event->name }}</span>
-                        <div class="d-flex flex-row align-items-center time-text"> <small>{{ $event->event_date . ' ' . $event->start_time }}</small> <span class="dots"></span> <small>viewed Just now</small> <span class="dots"></span> <small>Edited 15 minutes ago</small> </div>
+                        <div class="d-flex flex-row align-items-center time-text"> 
+                          <small>{{ $event->event_date . ' ' . $event->start_time }}</small> 
+                          {{-- <span class="dots"></span> <small>viewed Just now</small>  --}}
+                          {{-- <span class="dots"></span> <small>Edited 15 minutes ago</small> --}}
+                        </div>
                       </div>
                     </div>
                     <a class="btn btn-dark view-event-btn" href="{{ route('event.view', ['eventId' => $event->id]) }}">보기</a>
